78424150463a23227d4588163d191d6306e9eab7
df <df>
2016-10-03 15:33:28 +0000

    fix lights plugin

--- plugins/speechhandler/lights/lights.py
+++ plugins/speechhandler/lights/lights.py
@@ -2,13 +2,21 @@
 from jasper import plugin
 from subprocess import call
 import time
+import logging
 
 class LightsPlugin(plugin.SpeechHandlerPlugin):
-    COMMAND_LIGHTS_ON = 'pilight-send -p raw -c "294 1029 294 1029 882 441 294 1029 294 1029 882 441 294 1176 882 441 882 441 882 441 294 1029 882 441 294 1029 294 1176 882 441 294 1029 147 1029 882 441 882 441 147 1029 294 1029 294 1029 1029 441 294 1029 294 1029 294 1029 882 441 294 1029 294 4998"'
-    COMMAND_LIGHTS_OFF = 'pilight-send -p raw -c "294 1029 294 1176 882 294 294 1029 294 1029 882 441 882 441 294 1029 882 441 294 1029 294 1029 294 1029 882 441 294 1029 882 441 882 294 882 441 882 441 294 1029 294 1029 294 1029 882 441 294 1176 294 1029 882 441 882 441 882 441 294 1029 294 4998"'
+    COMMAND_LIGHTS_ON = 'pilight-send -p "rsl366" --systemcode=2 --programcode=2 --on'
+    COMMAND_LIGHTS_OFF = 'pilight-send -p "rsl366" --systemcode=2 --programcode=2 --off'
 
     def __init__(self, *args, **kwargs):
         super(LightsPlugin, self).__init__(*args, **kwargs)
+
+        self._logger = logging.getLogger(__name__)
+
+        try:
+            language = self.profile['language']
+        except KeyError:
+            language = 'en-US'
 
     def get_phrases(self):
         return self.get_phrases1() + self.get_phrases2()
@@ -20,28 +28,21 @@ class LightsPlugin(plugin.SpeechHandlerPlugin):
         return [self.gettext('ON'), self.gettext('OFF')]
 
     def handle(self, text, mic):
-        """
-        Responds to user-input, typically speech text, by telling a joke.
+	text = text.upper()
 
-        Arguments:
-        text -- user-input, typically transcribed speech
-        mic -- used to interact with the user (for both input and output)
-        """
         if self.gettext('ON') in text:
             for i in xrange(5):
-                call([self.COMMAND_LIGHTS_ON], shell=True)
+                self.sendcommand(self.COMMAND_LIGHTS_ON)
                 time.sleep(0.2)
 
         elif self.gettext('OFF') in text:
             for i in xrange(5):
-                call([self.COMMAND_LIGHTS_OFF], shell=True)
+                self.sendcommand(self.COMMAND_LIGHTS_OFF)
                 time.sleep(0.2)
 
-    def is_valid(self, text):
-        """
-        Returns True if the input is related to jokes/humor.
+    def sendcommand(self, command):
+        self._logger.info('sending command %s' % command)
+        call([command], shell=True)
 
-        Arguments:
-        text -- user-input, typically transcribed speech
-        """
+    def is_valid(self, text):
         return any(p.lower() == t.lower() for t in text.lower().split(' ') for p in self.get_phrases1()) and any(p.lower() == t.lower() for t in text.lower().split(' ') for p in self.get_phrases2())
